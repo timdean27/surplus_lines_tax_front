da76d7e72917164f04c975950f66b17177db1df7
timdean27 <timdean27@yahoo.com>
2022-12-06 16:10:21 -0500

    figuring out useeffect on Totals page

--- surplus_lines_tax_front/src/pages/StateTotals.jsx
+++ surplus_lines_tax_front/src/pages/StateTotals.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState , useEffect} from 'react'
 const StateTotals = ({StatePremiums, setStatePremiums}) => {
     console.log("state premiums in Totals page", StatePremiums)
 
     const [stateTotal, setStateTotal] = useState(null);
-    
+
     let hash = {}
+    const setStateTotalFucntion = ()=> {
     for(let i = 0; i < StatePremiums.length; i++){
       console.log(StatePremiums[i].state)
       
@@ -20,10 +21,28 @@ const StateTotals = ({StatePremiums, setStatePremiums}) => {
         console.log(StatePremiums[i].state)
       }
       console.log("hash",hash)
+      setStateTotal(hash)
+      console.log("stateTotal", stateTotal)
   }
+}
+
+useEffect(() => {
+  setStateTotalFucntion()
+},[hash]);
     
   return (
-    <div>StateTotals</div>
+    <div>
+    {/* {stateTotal.map(state, index) => (
+      <ul key={index}>
+        <li>ID:{state.id}</li>
+        <li>State:{state.state}</li>
+        <li>City:{state.city}</li>
+        <li>Zip:{state.zipcode}</li>
+        <li>Premium:{state.premium}</li>
+        <li>taxes due:{state.taxesDue}</li>
+      </ul>
+    )} */}
+    </div>
   )
 }
 
