b191f7b8ad51fd0377c137c99f6711da5ee7a833
timdean27 <timdean27@yahoo.com>
2022-12-06 14:46:58 -0500

    link to totals setting  premium state in app.js

--- surplus_lines_tax_front/src/App.jsx
+++ surplus_lines_tax_front/src/App.jsx
@@ -1,16 +1,42 @@
 import { useState } from 'react'
 import './App.css'
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
-
+import StateTotals from "./pages/StateTotals";
 
 function App() {
-
+  const [StatePremiums, setStatePremiums] = useState([
+    { id: 1, state: "New York", premium: 3000, taxesDue: 0 },
+    { id: 2, state: "New Jersey", premium: 4000, taxesDue: 0 },
+    { id: 3, state: "Florida", premium: 5000, taxesDue: 0 },
+  ]);
   return (
 
+
     <div className="app">
-    <Home/>
+          <Router>
+        <div className="app">
+          <Routes>
+          <Route
+            path="/"
+            element={<Home
+              StatePremiums={StatePremiums}
+              setStatePremiums={setStatePremiums}
+            />}
+          />
+            <Route
+              path="/state/totals"
+              element={
+                <StateTotals
+                  StatePremiums={StatePremiums}
+                  setStatePremiums={setStatePremiums}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+   
      </div>
   )
 }
